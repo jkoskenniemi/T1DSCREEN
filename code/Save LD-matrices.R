8431c8d98ce43987ba28043f1b3d65180383eb37
jkoskenniemi <122159221+jkoskenniemi@users.noreply.github.com>
2023-06-08 16:08:14 -0400

    New file to update the LD matrices

---
+++ code/Save LD-matrices.R
@@ -0,0 +1,45 @@
+
+rm(list=ls())
+#Get function import_data
+source("code/Functions.R")
+pacman::p_load(tidyverse, ieugwasr)
+
+
+#Names of available GWAS
+names_gwas <- c("IFNAR2",
+                "IL2RA",
+                "IL2RB",
+                # "IL12B",
+                # "IL2RG",
+                "IL6R",
+                # "IL6ST",
+                "JAK1",
+                "JAK2",
+                "JAK3",
+                "TYK2")
+
+#Save file paths
+names_eqtl <- paste0("data/export/", names_gwas, "_eqtl_TwoSampleMR.csv")
+names_t1d <- paste0("data/export/", names_gwas, "_T1D_TwoSampleMR.csv")
+
+#Harmonize all GWAS of eqtl and t1d and save them to a list
+harmonized_dfs <- map2(names_eqtl, names_t1d, import_data, has_beta = FALSE)
+
+#Add names of dfs to list
+names(harmonized_dfs)  <- names_gwas
+
+#Extract LD matrices (this takes quite a while)
+ld_matrices <- map(names_gwas, get_ld_matrix)
+
+#Trim the LD matrices
+trimmed_ld_matrices <- map2(ld_matrices, harmonized_dfs, trim_matrix)
+
+#Rename LD matrices
+names(trimmed_ld_matrices) <- paste0(names_gwas)
+
+#Write LD matrices
+map2(names_gwas, trimmed_ld_matrices, ~{
+  assign(.x, .y, envir = globalenv())
+  save(list = .x, file = paste0("data/export/", .x, "_LDmat.RData"))
+})
+
