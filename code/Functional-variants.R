eccee8ae6f862c870d8770d7691e98327ad7b113
jkoskenniemi <122159221+jkoskenniemi@users.noreply.github.com>
2023-06-19 10:33:58 -0400

    Create Functional-variants.R
    first commit

---
+++ code/Functional-variants.R
@@ -0,0 +1,44 @@
+
+#Load packages
+
+#If run for the first time
+# install.packages("devtools")
+# library(devtools)
+# install_github("phenoscanner/phenoscanner")
+# library(phenoscanner)
+
+#Load packages
+pacman::p_load(tidyverse, phenoscanner)
+
+names_gwas <- c("IFNAR2", "IL2RA", "IL2RB",
+                "IL12B", "IL2RG", "IL6R",
+                "IL6ST", "JAK1", "JAK2",
+                "JAK3", "TYK2")
+
+# functional_variants <- map(names_gwas, ~phenoscanner(genequery = .x))
+# names(functional_variants) <- names_gwas
+
+# save(functional_variants, file = "data/export/phenoscanner-functional-variants.RDS")
+load("data/export/phenoscanner-functional-variants.RDS")
+
+
+#Select only $results (i.e. flatten)
+functional_variants_flattened <- sapply(functional_variants, function(x) x[2])
+
+#Change to data.frame
+df <- bind_rows(functional_variants_flattened)
+
+#Select only diabetes variants
+df %>%
+  filter(str_detect(trait, "diabetes")) %>%
+  select(gene, rsid, eur, consequence, protein_position, amino_acids, trait, efo, study, pmid)
+
+df %>%
+  filter(consequence == "missense") %>%
+  filter(duplicated(rsid) == FALSE) %>%
+  select(rsid, gene, hg38_coordinates, eur, consequence, protein_position, amino_acids)
+
+save(functional_variants, file = "data/export/phenoscanner-functional-variants-rsids.RDS")
+
+
+
