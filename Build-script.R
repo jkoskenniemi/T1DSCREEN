b58db2c6c03f37b5059d062db663fad6dd5f6cb2
Jaakko Koskenniemi <jajoko@utu.fi>
2024-05-22 08:54:42 -0400

    Added plink-path and a few packages.

--- Build-script.R
+++ Build-script.R
@@ -1,15 +1,25 @@
 #Build script
 
-#if needed:
+#You need Plink installed in your system to run these analyses
+#Set path to Plink here:
+plink_path = "~/Documents/plinkref/EUR"
+
+#if any of these tools is not installed:
 #install.packages("pacman")
-#install.packages("devtools")
-#devtools::install_github("phenoscanner")
-#install_github("phenoscanner/phenoscanner")
-pacman::p_load(data.table, dtplyr, dplyr, tidyverse, rio, here, TwoSampleMR, phenoscanner, phenoscanner, purrr)
+install.packages("devtools")
+devtools::install_github("phenoscanner/phenoscanner")
+install.packages("remotes")
+remotes::install_github("MRCIEU/TwoSampleMR")
+remotes::install_github("MRCIEU/genetics.binaRies")
+
+#Load packages
+pacman::p_load(data.table, dtplyr, dplyr, tidyverse, rio, here, TwoSampleMR,
+               phenoscanner, phenoscanner, purrr, ieugwasr, genetics.binaRies, coloc)
+
 
 
 source("code/01-Functions.R")
 source("code/02-Gather.R")
 source("code/03-Harmonize.R")
-source("code/04-Save-LD-matrices.R")
-source("code/05-Functional-variants.R")
+source("code/04-Functional-variants.R")
+source("code/05-Save-LD-matrices.R")
